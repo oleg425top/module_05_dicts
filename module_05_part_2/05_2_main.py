4b37b40c61eeefb8548888266dfb9a9232312249
oleg425top <oleg59152@gmail.com>
2024-11-28 18:01:45 +0500

    дописал переменные согласно образцу. Свой вариант закомитил

--- module_05_part_2/05_2_main.py
+++ module_05_part_2/05_2_main.py
@@ -1,5 +1,10 @@
 from module_05_part_2 import *
 
 user_lvl = input("Выберите уровень сложности \nлегкий, средний, сложный: \n").lower()
+test_words = get_user_level(user_lvl)
+test_answers = base_program(test_words)
+result = get_result(test_answers)
+print(f"\nВаш ранг:\n{result}")
 
-print(get_result(base_program(get_user_level(user_lvl))))
+
+# print(get_result(base_program(get_user_level(user_lvl))))
