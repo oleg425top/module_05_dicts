6963e848ecdb9d5ce79943111924f02ae5d57c53
oleg425top <oleg59152@gmail.com>
2024-11-28 22:19:43 +0500

    добавил создание json файла

--- module_05_part_2/05_2_main.py
+++ module_05_part_2/05_2_main.py
@@ -1,5 +1,6 @@
+import json
 from module_05_part_2 import *
-
+name = input('Введите ваше имя')
 user_lvl = input("Выберите уровень сложности \nлегкий, средний, сложный: \n").lower()
 test_words = get_user_level(user_lvl)
 test_answers = base_program(test_words)
@@ -7,4 +8,8 @@ result = get_result(test_answers)
 print(f"\nВаш ранг:\n{result}")
 
 
+
+json_data = json.dumps(test_answers, ensure_ascii=False, indent=4)
+with open(f'{name}.json', 'w', encoding='utf-8') as file:
+    file.write(json_data)
 # print(get_result(base_program(get_user_level(user_lvl))))
